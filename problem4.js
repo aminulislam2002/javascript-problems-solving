dea92d568d9fcae09aa8517ffd352cc8b9efa73d
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-08-29 09:55:48 -0700

    solve problem4

---
+++ problem4.js
@@ -0,0 +1,14 @@
+// Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+// Calculate the area
+function calculateTriangleArea(base, height) {
+  return (area = (base * height) / 2);
+}
+
+// Example usage:
+const base = 5;
+const height = 8;
+const triangleArea = calculateTriangleArea(base, height);
+
+console.log("The area of the triangle is: " + triangleArea);
